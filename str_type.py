a98827dccb7f001eaf7e56b0911a65ffa8ebdf90
MUKESH <mukesh.ora@outlook.com>
2019-09-30 00:21:09 +0530

    1st string oprations added by Joker

--- str_type.py
+++ str_type.py
@@ -1,1 +1,27 @@
+#print a string with quotes
+s = 'Joker'
+print(s)
+print(repr(s))
 
+
+# To find duplicate value
+
+from collections import Counter
+
+def dup_str(string):
+    f_count = Counter(string)
+    for k in f_count.keys():
+        if f_count.get(k)>1:
+            print(k + ","+str(f_count.get(k)))
+mystr = 'Jason bbourne'
+a = dup_str(mystr)
+print(a)
+
+# find out common letter between two strings
+
+user_str1 = input('enter string 1:')
+user_str2 = input('enter string 2:')
+s1 = set(user_str1)
+s2 = set(user_str2)
+lst = list(s1 & s2)
+print('common letter : {}'.format(lst))
