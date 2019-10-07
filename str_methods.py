cfc46cc2ae237937cdbed0b2c8739b56dacec87a
MUKESH <mukesh.ora@outlook.com>
2019-10-07 20:07:12 +0530

    Adedd by MK

---
+++ str_methods.py
@@ -0,0 +1,43 @@
+#Case conversion
+#capitalize() method
+# It will convert 1st charecter in upper case and remaining Lower case
+s = 'joKer'
+s1 = 'joker'
+
+print(s.capitalize())
+print(s1.capitalize())
+
+s2 = 'joker1323#MK'
+print(s2.capitalize())
+
+#OP-Joker1323#mk
+
+# Lower () method
+#It  will convert all charecter in lower case
+s = 'JoKeR'
+print(s.lower())
+s1 = 'WHY_SO_SERIOUS'
+print(s1.lower())
+
+# swapcase()
+#It will converted upper case letter to lower case and vice versa
+
+s = 'aMzinG_SpiDerMan'
+print(s.swapcase())
+#OP-AmZINg_sPIdERmAN
+
+# title()
+# It will return 1st letter of each word into uppercase and remaining into lowercase
+
+s = 'aMAZING_joKER'
+print(s.title())
+#OP-Amazing_Joker
+print("what's happend to the joker after he destroy batman".title())
+#OP-What'S Happend To The Joker After He Destroy Batman
+
+# Upper()
+#It will convert all charecter into uppercase
+s = 'why_so_serious'
+print(s.upper())
+#one more way to convert
+print('why_so_Serious'.upper())
